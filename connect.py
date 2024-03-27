d4239f0cfdaa15775acc832fb75fa5d02f0b28ae
TonnyMpho <tonnympho1@gmail.com>
2024-03-27 23:32:43 +0200

    file for testing MongoDB connection

--- connect.py
+++ connect.py
@@ -1,13 +1,8 @@
+""" file for testing my database """
 from pymongo import MongoClient
-from datetime import datetime
-import bcrypt
 
 
 client = MongoClient('mongodb://192.168.56.1:27017')
-
-#password = bcrypt.hashpw('secret'.encode('utf-8'), bcrypt.gensalt())
-
-#client.expense_tracker.users.delete_one({'username': 'cate1'})
 
 users = client.expense_tracker.users.find()
 expenses = client.expense_tracker.expenses.find()
